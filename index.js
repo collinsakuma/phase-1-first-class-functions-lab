5483983d06295cb1a38b22ac3667a639a7e8d5fa
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-01 14:44:04 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (drivers) => {
+    return [...drivers.slice(0,2)];
+}
+
+const returnLastTwoDrivers = drivers => {
+    return [...drivers.slice(2)];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers ]; 
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return multiplier * fare;
+    }
+}
+
+const fareMulti = createFareMultiplier(2);
+function fareDoubler(fare) {
+    return fareMulti(fare);
+}
+
+const fareMulti2 = createFareMultiplier(3);
+function fareTripler(fare) {
+    return fareMulti2(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, funct) {
+    return funct(arrayOfDrivers);
+}
